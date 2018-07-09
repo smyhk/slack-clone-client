d2a37d92f51813c0732f71cf27fdaede99143f26
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-07-09 11:51:00 -0700

    Initial commit and setup

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import gql from "graphql-tag";
+import { graphql } from 'react-apollo';
+
+const allUsersQuery = gql`
+  {
+    allUsers {
+      id
+      username
+    }
+  }
+`;
+
+class Home extends Component {
+  render() {
+    const { data: { loading, allUsers } } = this.props;
+    if (loading) {
+      return null;
+    }
+    return (
+      <div>
+        {
+          allUsers.map(u => (
+            <h1 key={u.id}>{u.username}</h1>
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+export default graphql(allUsersQuery)(Home);
